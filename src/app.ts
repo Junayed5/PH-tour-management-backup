c5664abf09c63aa77ca8dc8017db950e66200e0c
Junayed5 <mdjunayed352003@gmail.com>
2025-09-03 23:46:29 +0600

    user controller and routes created

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express'
+import { userRoutes } from './app/modules/user/user.routes';
+import cors from 'cors'
 
 export const app = express();
+app.use(express.json())
+app.use('/api/v1/user', userRoutes)
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({
