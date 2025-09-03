ecc596ef1fe2e36d90f59c8b0b5b2f68763ac12a
Junayed5 <mdjunayed352003@gmail.com>
2025-09-03 23:07:05 +0600

    user interfaces

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,42 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+}
+
+// auth provider
+/**
+ * email, password
+ * google authentication
+ */
+
+export interface IAuthProvider {
+  provider: string;
+  proverId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified?: string;
+  role: Role;
+  auths: IAuthProvider[];
+
+  booking?: Types.ObjectId[];
+  guide?: Types.ObjectId[];
+}
